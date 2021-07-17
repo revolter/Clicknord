1d9ae13ee9d977ed2e3086a81e103dfd202a1f6b
Iulian Onofrei <iulian.onofrei@yahoo.com>
2021-07-17 22:18:32 +0100

    Refactor: Replaced ifs with guards

--- src/popup.js
+++ src/popup.js
@@ -3,37 +3,41 @@ function openHqqTVVideo() {
 		iframe = document.querySelector('iframe[src*="hqq.tv"]'),
 		hash = '#clicknord';
 
-	if (iframe && !window.location.href.endsWith(hash)) {
-		iframe.style.width = '100%';
-		iframe.style.height = '100%';
-
-		document.documentElement.style.height = '100%';
-		document.body.style.height = '100%';
-
-		document.body.innerHTML = iframe.outerHTML;
-
-		window.addEventListener('hashchange', function (event) {
-			if (event.oldURL.endsWith(hash)) {
-				window.location.reload();
-			}
-		});
-
-		window.history.pushState(null, null, hash);
-	} else {
+	if (iframe === null || window.location.href.endsWith(hash)) {
 		alert('Nu a fost găsit server-ul I.');
+
+		return;
 	}
+
+	iframe.style.width = '100%';
+	iframe.style.height = '100%';
+
+	document.documentElement.style.height = '100%';
+	document.body.style.height = '100%';
+
+	document.body.innerHTML = iframe.outerHTML;
+
+	window.addEventListener('hashchange', function (event) {
+		if (event.oldURL.endsWith(hash)) {
+			window.location.reload();
+		}
+	});
+
+	window.history.pushState(null, null, hash);
 }
 
 function openOkRuVideo() {
 	const iframe = document.querySelector('iframe[src*="ok.ru"]');
 
-	if (iframe) {
-		const url = iframe.src;
-
-		window.location = url;
-	} else {
+	if (iframe === null) {
 		alert('Nu a fost găsit server-ul II.');
+
+		return;
 	}
+
+	const url = iframe.src;
+
+	window.location = url;
 }
 
 function main() {
